46f1e4304cf258d1c2c2c4c51f37899265257297
TommcyOWO <tangminghong.tom@gmail.com>
2023-10-09 20:53:59 +0800

    update

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="h:80px flex jc:space-between ai:center color:sky-74 p:10px f:20 f:semibold">
+    <div class="h:80px flex ai:center color:sky-74 p:10px f:20 f:semibold">
         <IconMenu2 @click="menu()" class="hide@>md mx:20px cursor:pointer"/>
             <ul class="lt:none hide@<md flex ai:center mx:20px">
                 <li @click="moveTo('index')" class="cursor:pointer inline mx:20px shadow:0|2|sky-80:hover ~300ms|ease-in">首頁</li>
--- frontend/pages/search/index.vue
+++ frontend/pages/search/index.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="m:30px">
-    <h1>課外活動：</h1>
-    {{ result }}
-    <div>
+    <div class="flex jc:space-between ai:center">
+      <h1>課外活動：</h1>
+      <div class="flex rel ai:center bg:sky-76 r:10">
+        <IconSearch color="white" class="m:5px|0px|5px|5px cursor:pointer"/>
+        <input v-model="search_input" class="mr:10px b:none outline:none r:3px" @input="fun_search" type="text">
+      </div>
     </div>
+    {{ result }}
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import '@master/css'
+import { IconSearch } from '@tabler/icons-vue';
+
 const { result, search } = useAlgoliaSearch('CSG') // pass your index name as param
-onMounted(async () => {
-  await search({ query: 'Samsung' });
-})
+
+const search_input = ref('')
+
+const search_key = useMagicKeys()['Enter']
+
+const fun_search = async () => {
+  await search({ query: search_input.value });
+}
+
 </script>
 
 <style scoped></style>
